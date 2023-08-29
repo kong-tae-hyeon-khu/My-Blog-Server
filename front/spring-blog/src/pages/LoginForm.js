2c9c1fdc584bd476111f128b2a84e2275451d729
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-08-29 22:26:30 +0900

    Feat : Login Page

---
+++ front/spring-blog/src/pages/LoginForm.js
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import styles from '../styles/LoginForm.module.css';
+import Button from "../components/Button";
+
+
+const LoginForm = () => {
+
+
+    const [Email, setId] = useState("");
+    const [Password, setPassword] = useState("");
+
+    const onIdHandler = (event) => {
+        setId(event.currentTarget.value);
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    }
+
+    const onSubmitHandler = async (event) => {
+        event.preventDefault(); // Prevent Page Reload
+
+        console.log('Email : ' , Email)
+        console.log('Password : ', Password)
+
+
+        let body = {
+            email : Email,
+            password : Password
+        }
+
+        try {
+            const response = await axios.post('http://localhost:8080/user/signin', body);
+
+            if (response.status === 200) {
+                console.log("로그인 성공", response)
+            }
+            else {
+                console.log("로그인 실패", response)
+            }
+        } catch (err) {
+            console.log("에러 ", err)
+        }
+
+
+    }
+
+    return (
+        <div className={styles.container}>
+            <form onSubmit={onSubmitHandler} className={styles.form}>
+                <label className={styles.label}>ID</label>
+                <input value={Email} onChange={onIdHandler} className={styles.input}/>
+
+                <br/>
+
+                <label className={styles.label}>Password</label>
+                <input value={Password} onChange={onPasswordHandler} className={styles.input}/>
+
+                <br/>
+                <Button onClick={onSubmitHandler}>Login</Button>
+
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
