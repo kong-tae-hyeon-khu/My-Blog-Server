c6588f477c6b60bb6d6b8ad3283cda340095d047
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-08-30 21:34:11 +0900

    Feat : Add Routing

---
+++ front/spring-blog/src/pages/RegisterForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RegisterForm = () => {
+    return (
+        <div>
+            <form>
+                <label></label>
+                <input></input>
+            </form>
+        </div>
+    )
+}
+
+export default RegisterForm;
