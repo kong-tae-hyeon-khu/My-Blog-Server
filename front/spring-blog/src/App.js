8e6370d5d7ade00c7e86134560e4e69ecdd84a3d
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-09-15 14:01:51 +0900

    Feat : Logout (Using Cookie)

--- front/spring-blog/src/App.js
+++ front/spring-blog/src/App.js
@@ -3,15 +3,18 @@ import LoginForm from "./pages/LoginForm";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import RegisterForm from "./pages/RegisterForm";
 import Main from "./pages/Main";
-
+import {getCookie} from "./settings/cookie";
 function App() {
+
+    const toekn = getCookie("jwt");
+
     return (
         <BrowserRouter>
             <div>
                 <Routes>
-                    <Route path="signin" element={<LoginForm/>}></Route>
+                    <Route path="signin" element={<LoginForm/>}></Route>t
                     <Route path="signup" element={<RegisterForm/>}></Route>
-                    <Route path="/" element={<Main/>}></Route>
+                    <Route path="/" element={toekn ? <Main/> : <LoginForm/>}></Route>
                 </Routes>
 
             </div>
--- front/spring-blog/src/pages/Main.js
+++ front/spring-blog/src/pages/Main.js
@@ -1,15 +1,23 @@
 import React from 'react'
-import {getCookie} from "../settings/cookie";
+import {getCookie, removeCookie} from "../settings/cookie";
+import Button from "../components/Button";
 
 const Main = () => {
 
     const onClickGetCookie = () => {
         console.log(getCookie("jwt"));
     }
+
+    const onClickLogout = () => {
+        removeCookie("jwt");
+        window.location.reload();
+    }
+
     return (
         <div>
             "메인 페이지"
-            <button onClick={onClickGetCookie}></button>
+            <button onClick={onClickGetCookie}>버튼</button>
+            <Button onClick={onClickLogout}>로그아웃</Button>
         </div>
     )
 }
