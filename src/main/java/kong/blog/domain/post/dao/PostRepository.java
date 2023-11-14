c113e43e7f118dc1503e652b998ee43f3b3b7a03
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-11-14 16:41:43 +0900

    Feat : Paging Post

--- src/main/java/kong/blog/domain/post/dao/PostRepository.java
+++ src/main/java/kong/blog/domain/post/dao/PostRepository.java
@@ -2,6 +2,8 @@ package kong.blog.domain.post.dao;
 
 import kong.blog.domain.post.domain.Post;
 import kong.blog.domain.user.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,8 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByUser(User user);
+
+    // Paging
+    Page<Post> findAll(Pageable pageable);
+
 }
